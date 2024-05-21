96a7d406c2205c02a3575220c8798ee7a06a2ad1
SebastianK2000 <sebastian.kosciolek2000@gmail.com>
2024-05-21 17:43:45 +0200

    Create page gifts and i use Tailwind

---
+++ src/data/mockData.tsx
@@ -0,0 +1,125 @@
+import img1 from '../../Resources/1.jpg';
+import img2 from '../../Resources/2.jpg';
+import img3 from '../../Resources/3.jpg';
+import img4 from '../../Resources/4.jpg';
+import img5 from '../../Resources/5.jpg';
+import img6 from '../../Resources/6.jpg';
+import img7 from '../../Resources/7.jpg';
+import img8 from '../../Resources/8.jpg';
+import img9 from '../../Resources/9.jpg';
+import img10 from '../../Resources/10.jpg';
+import img11 from '../../Resources/11.jpg';
+import img12 from '../../Resources/12.jpg';
+import img13 from '../../Resources/13.jpg';
+import img14 from '../../Resources/14.jpg';
+import img15 from '../../Resources/15.jpg';
+import img16 from '../../Resources/16.jpg';
+import img17 from '../../Resources/17.jpg';
+import img18 from '../../Resources/18.jpg';
+import img19 from '../../Resources/19.jpg';
+import img20 from '../../Resources/20.jpg';
+import img21 from '../../Resources/21.jpg';
+import img22 from '../../Resources/22.jpg';
+import img23 from '../../Resources/23.jpg';
+import img24 from '../../Resources/24.jpg';
+import img25 from '../../Resources/25.jpg';
+import img26 from '../../Resources/26.jpg';
+
+
+export const data = [
+    {
+        id: 1,
+        img: img1,
+    },
+    {
+        id: 2,
+        img: img2,
+    },
+    {
+        id: 3,
+        img: img3,
+    },
+    {
+        id: 4,
+        img: img4,
+    },
+    {
+        id: 5,
+        img: img5,
+    },
+    {
+        id: 6,
+        img: img6,
+    },
+    {
+        id: 7,
+        img: img7,
+    },
+    {
+        id: 8,
+        img: img8,
+    },
+    {
+        id: 9,
+        img: img9,
+    },
+    {
+        id: 10,
+        img: img10,
+    },
+    {
+        id: 11,
+        img: img11,
+    },
+    {
+        id: 12,
+        img: img12,
+    },
+    {
+        id: 13,
+        img: img13,
+    },
+    {
+        id: 14,
+        img: img14,
+    },
+    {
+        id: 15,
+        img: img15,
+    }, {
+        id: 16,
+        img: img16,
+    }, {
+        id: 17,
+        img: img17,
+    },
+    {
+        id: 18,
+        img: img18,
+    }, {
+        id: 19,
+        img: img19,
+    }, {
+        id: 20,
+        img: img20,
+    }, {
+        id: 21,
+        img: img21,
+    }, {
+        id: 22,
+        img: img22,
+    }, {
+        id: 23,
+        img: img23,
+    }, {
+        id: 24,
+        img: img24,
+    },
+    {
+        id: 25,
+        img: img25,
+    }, {
+        id: 26,
+        img: img26,
+    },
+];
--- src/pages/Gifts.tsx
+++ src/pages/Gifts.tsx
@@ -1,11 +1,98 @@
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { data } from '../data/mockData';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import '../style/gifts.css';
 
+interface DataItem {
+    img: string;
+}
 
+export function Gifts() {
+    const sliderRef = useRef<HTMLDivElement | null>(null);
 
-export const Gifts = () => {
+    const slideLeft = () => {
+        const slider = sliderRef.current;
+        if (slider) {
+            if (slider.scrollLeft === 0) {
+                slider.scrollLeft = slider.scrollWidth - slider.clientWidth;
+            } else {
+                slider.scrollLeft -= 500;
+            }
+        }
+    };
+
+    const slideRight = () => {
+        const slider = sliderRef.current;
+        if (slider) {
+            if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+                slider.scrollLeft = 0;
+            } else {
+                slider.scrollLeft += 500;
+            }
+        }
+    };
+
+    const items = [...data, ...data, ...data];
+
+    const itemStyle = {
+        width: '300px',
+        height: '400px'
+    };
+
+    useEffect(() => {
+        const slider = sliderRef.current;
+        if (slider) {
+            const handleScroll = () => {
+                if (slider.scrollLeft === 0) {
+                    slider.scrollLeft = slider.scrollWidth / 3;
+                } else if (slider.scrollLeft + slider.clientWidth >= slider.scrollWidth) {
+                    slider.scrollLeft = slider.scrollWidth / 3;
+                }
+            };
+
+            slider.addEventListener('scroll', handleScroll);
+            slider.scrollLeft = slider.scrollWidth / 3;
+
+            return () => {
+                slider.removeEventListener('scroll', handleScroll);
+            };
+        }
+    }, []);
+
     return (
-            <Outlet />
-    )
+        <>
+            <img
+                style={{ borderRadius: '10px' }}
+                className='w-full h-[440px] object-cover'
+                src='../Resources/frameGift.png'
+                alt='Cover'
+            />
+
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '10px' }}>
+                <h2 style={{  }}>Choose a Book</h2>
+            </div>
+
+            <div style={{ margin: '20px' }} className='relative flex items-center'>
+                <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideLeft} size={40} />
+                <div
+                    ref={sliderRef}
+                    id='slider'
+                    className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide'
+                >
+                    {items.map((item: DataItem, index: number) => (
+                        <img
+                            key={index}
+                            style={itemStyle}
+                            className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300'
+                            src={item.img}
+                            alt={`Item ${index}`}
+                        />
+                    ))}
+                </div>
+                <MdChevronRight className='opacity-50 cursor-pointer hover:opacity-100' onClick={slideRight} size={40} />
+            </div>
+        </>
+    );
 }
 
 export default Gifts;
